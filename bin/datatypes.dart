c2b5d02787dd47454cb4996014fc4f72f213a0f3
ABDUL REHMAN <mr.abdulrehman.ar@gmail.com>
2021-10-01 16:59:52 +0500

    Initial commit

---
+++ bin/datatypes.dart
@@ -0,0 +1,20 @@
+//Data Types in dart
+//Numbers in dart ==> real numbers , floating point numbers
+//real numbers
+int personAge = 15;
+int marks = -25;
+//floating point numbers
+double cgpa = 3.55;
+//String ==> store characters, set of charcters 
+String fullName = 'Ali';
+String char = 'A';
+// boolean ==> store true or false value
+bool checkLogin = false;
+//List ==> store multiple values of same data
+List<String> studentNames = ['Ali','Ahmad','Hassan','4545'];
+// Map ==> manage tha api or database data
+
+// dynamic ==>  use when data is unknown
+dynamic lastName = 'Rehman';
+// var ==> store random data
+const country = 'Pakistan';
